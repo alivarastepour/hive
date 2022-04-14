fb17a4009f897169336e0ec384812effccb60a0e
Ali <alivaraste@outlook.com>
2022-04-14 11:40:29 +0430

    Game class init

---
+++ Game.py
@@ -0,0 +1,5 @@
+class Game:
+    def __init__(self):
+        self.selectedHexagon = ''  # determines which hexagon is selected
+        self.selectedPiece = ''  # determines which piece is selected
+        self.turn = ''  # determines whose turn is it
