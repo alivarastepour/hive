7d6130d2188a17e27f95330cb0275246fab279f5
Ali <alivaraste@outlook.com>
2022-04-14 12:52:20 +0430

    more logic for used and available pieces are added to User class

--- User.py
+++ User.py
@@ -1,6 +1,34 @@
+from Piece import Piece
+from constants import QUEEN, BEETLE, ANT, GRASSHOPPER, SPIDER
+
+
 class User:
     def __init__(self, userId):
         self.id = userId
-        self.availablePieces = dict({'queen': 1, 'beetle': 2, 'ant': 3, 'spider': 2,
-                                     'grasshopper': 3, })  # keeps track of individual user's remaining Pieces
+        self.availablePieces = dict(
+            {
+                QUEEN: [Piece(QUEEN, 1)],
+                BEETLE: [Piece(BEETLE, i) for i in range(1, 3)],
+                ANT: [Piece(ANT, i) for i in range(1, 4)],
+                SPIDER: [Piece(SPIDER, i) for i in range(1, 3)],
+                GRASSHOPPER: [Piece(GRASSHOPPER, i) for i in range(1, 4)],
+            }
+        )  # keeps track of individual user's remaining Pieces
+        self.piecesPlacement = dict(
+            {
+                QUEEN: [None],
+                BEETLE: [None, None],
+                ANT: [None, None, None],
+                SPIDER: [None, None],
+                GRASSHOPPER: [None, None, None],
+            }
+        )
         self.hasEnteredTheQueen = False  # determines whether individual user's queen has been moved to the board
+
+    def usePiece(self, piece, placement, game):
+        try:
+            self.availablePieces.get(piece).pop(0)
+        except IndexError:
+            game.error = 'you have no {} left'.format(piece)
+            return
+        # self.piecesPlacement.update({piece:self.piecesPlacement.get(piece)[]})
