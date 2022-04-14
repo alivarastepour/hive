04c495387f15c5aad585ee5c45493f299308f2b1
Ali <alivaraste@outlook.com>
2022-04-14 12:49:59 +0430

    error property is added

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
 class Game:
     def __init__(self):
-        self.selectedHexagon = ''  # determines which hexagon is selected
-        self.selectedPiece = ''  # determines which piece is selected
-        self.turn = ''  # determines whose turn is it
+        self.__selectedHexagon = ''  # determines which hexagon is selected
+        self.__selectedPiece = ''  # determines which piece is selected
+        self.__turn = ''  # determines whose turn is it
+        self.error = ''  # determines whether there is an error or not
