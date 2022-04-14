6e3ee903de21cfce0ca8c0bd211f815b2197a4f1
Ali <alivaraste@outlook.com>
2022-04-14 14:27:08 +0430

    middleware for selectedHexagon is added

--- Game.py
+++ Game.py
@@ -1,6 +1,15 @@
 class Game:
     def __init__(self):
         self.__selectedHexagon = ''  # determines which hexagon is selected
-        self.__selectedPiece = ''  # determines which piece is selected
-        self.__turn = ''  # determines whose turn is it
+        self.selectedPiece = ''  # determines which piece is selected
+        self.turn = ''  # determines whose turn is it
         self.error = ''  # determines whether there is an error or not
+
+    @property
+    def selectedHexagon(self):
+        return self.__selectedHexagon
+
+    @selectedHexagon.setter
+    def selectedHexagon(self, val):
+        print(val)
+        self.__selectedHexagon = val
