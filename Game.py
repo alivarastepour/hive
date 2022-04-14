c122ee03c3066b11048677afcf563740c311f553
Ali <alivaraste@outlook.com>
2022-04-14 18:43:33 +0430

    board of game is initialized

--- Game.py
+++ Game.py
@@ -1,15 +1,18 @@
+def createBoard():
+    board = []
+    for a in range(22):
+        temp = []
+        for b in range(22):
+            temp.append([])
+        board.append(temp)
+    print(board)
+    return board
+
+
 class Game:
     def __init__(self):
         self.__selectedHexagon = ''  # determines which hexagon is selected
         self.selectedPiece = ''  # determines which piece is selected
         self.turn = ''  # determines whose turn is it
         self.error = ''  # determines whether there is an error or not
-
-    @property
-    def selectedHexagon(self):
-        return self.__selectedHexagon
-
-    @selectedHexagon.setter
-    def selectedHexagon(self, val):
-        print(val)
-        self.__selectedHexagon = val
+        self.board = createBoard()  # keeps track of game's board
