1eff9ae5f06c9c0a9d85b6cbb43b715a00d41382
Ali <alivaraste@outlook.com>
2022-04-15 15:16:37 +0430

    a function for calculating grasshopper's valid moves is added

--- Game.py
+++ Game.py
@@ -1,3 +1,6 @@
+from neighborhoodUtil import *
+
+
 def createBoard():  # inits the game's board
     board = []
     for a in range(22):
@@ -6,12 +9,6 @@ def createBoard():  # inits the game's board
             temp.append([])
         board.append(temp)
     return board
-
-
-def getNeighbors(x, y):  # returns neighbors of a house
-    return [(x - 1 if y % 2 == 1 else x, y - 1), (x - 1 if y % 2 == 1 else x - 1, y),
-            (x - 1 if y % 2 == 1 else x, y + 1), (x + 1 if y % 2 == 0 else x, y - 1),
-            (x + 1 if y % 2 == 0 else x + 1, y), (x + 1 if y % 2 == 0 else x, y + 1)]
 
 
 class Game:
@@ -23,20 +20,6 @@ class Game:
         self.board = createBoard()  # keeps track of game's board
         self.isFirstMove = True  # determines whether this is the first move
         self.occupiedHomes = dict()  # keeps track of occupied homes
-
-    # def getBoundary(self):
-    #     minX, maxX, minY, maxY = 21, 0, 21, 0
-    #     for home in self.occupiedHomes:
-    #         user, piece, x, y = home
-    #         if x > maxX:
-    #             maxX = x
-    #         if x < minX:
-    #             minX = x
-    #         if y > maxY:
-    #             maxY = y
-    #         if y < minY:
-    #             minY = y
-    #     return (minX, maxX), (minY, maxY)
 
     def availablePositions(self):  # calculates available positions for a user
         arr = []
@@ -66,3 +49,21 @@ class Game:
         self.occupiedHomes.update({(x, y): (user, piece)})
         if self.isFirstMove:
             self.isFirstMove = False
+
+    def grasshopperValidMoves(self, x, y):  # returns all valid positions for a grasshopper
+        validHomes = []
+        if (x, y) not in self.occupiedHomes.keys():
+            self.error = 'this home does not contain a grasshopper'
+            return
+        neighbors = getNeighborsWithDirection(x, y)
+        for neighbor in neighbors:
+            direction, x, y = neighbor
+            if not (x, y) in self.occupiedHomes.keys():
+                continue
+            while True:
+                possibleHome = getCoordinates(x, y, direction)
+                if possibleHome not in self.occupiedHomes.keys():
+                    validHomes.append(possibleHome)
+                    break
+                x, y = getCoordinates(x, y, direction)
+        return validHomes
